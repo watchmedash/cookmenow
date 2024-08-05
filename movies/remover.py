90544856bded38967afb2b6eb8218b3b36136381
watchmedash <mojazer2017@gmail.com>
2024-08-05 11:54:13 +0400

    up

--- movies/remover.py
+++ movies/remover.py
@@ -5,17 +5,7 @@ directory = r'C:\Users\mojaz\Documents\GitHub\cookmenow\movies'  # Replace with 
 
 # Block to be removed
 block_to_remove = """
-<header>
-<nav>
-<div class="logo-title-container">
-<div class="logo-container"><img alt="Nextflix Logo" src="https://i.postimg.cc/Kv4RxRFw/logo1.png"/></div>
-<div class="nav-title"><h1>Nextflix</h1><p>Stream Free, Stream Now</p></div>
-</div><div class="nav-links"><ul>
-<li><a href="../home.html"><i class="fas fa-home"></i>Home</a></li>
-<li><a href="../contact.html"><i class="fas fa-envelope"></i>Contact Us</a></li>
-<li><a href="../about.html"><i class="fas fa-info-circle"></i>About Us</a></li>
-</ul></div></nav>
-</header>
+ - Watch Free Movies and TV Series Online
 """
 
 def remove_block_from_html(directory, block):
