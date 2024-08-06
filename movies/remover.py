be71ab074fed934a0a8e5a89edb73f0f0cfc71dd
watchmedash <mojazer2017@gmail.com>
2024-08-06 14:08:50 +0400

    up

--- movies/remover.py
+++ movies/remover.py
@@ -5,48 +5,8 @@ directory = r'C:\Users\HP\Documents\GitHub\cookmenow\movies'  # Replace with you
 
 # Block to be removed
 block_to_remove = """
-<div class="sharethis-inline-share-buttons"></div>
-<!--<div class="discontent"><h3>Choose server:</h3></div>
-  <div class="tabs">
-  <div class="custom-tab" onclick="switchVideo('server1', 'video-player', 'https://filemoon.sx/e/olphc307bfpy')">Filemoon</div>
-  <div class="custom-tab" onclick="switchVideo('server2', 'video-player', 'https://vidsrc.me/embed/movie/tt6263850')">Streamy</div>
-  <div class="custom-tab" onclick="switchVideo('server3', 'video-player', 'https://multiembed.mov/directstream.php?video_id=tt6791350')">Streamify</div>
-   -->
-<div class="button-container"><a class="watch-now-bur" href="../contact.html">Report Error!</a></div>
-<div class="content"><div class="watchmenow-info"><h1>What to watch next? 🎥</h1></div></div><div class="related-movies-gallery">
-</div>
-<div id="disqus_thread"></div>
-<script>
-    var disqus_config = function () {
-        this.page.url = window.location.href;  // Use the current page URL
-        this.page.identifier = document.title; // Use the page title as the unique identifier
-    };
-
-    (function() { // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
-        s.src = 'https://watchmenow.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
+<script async="" src="https://platform-api.sharethis.com/js/sharethis.js#property=64b0f5079f35fb00121494d9&amp;product=sticky-share-buttons">
 </script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-<footer>
-<nav class="footer-nav">
-<ul class="footer-links">
-<li><a href="../home.html">Home</a></li>
-<li><a href="../contact.html">Contact Us</a></li>
-<li><a href="../privacy policy.html">Privacy Policy</a></li>
-<li><a href="../dmca.html">DMCA</a></li>
-</ul>
-<div class="social-icons">
-<a class="social-icon" href="https://perilastronaut.com/drgbi6nk3?key=b9a8423ba82179e793ae4a5b1096f541"><i class="fab fa-instagram"></i></a>
-<a class="social-icon" href="https://perilastronaut.com/drgbi6nk3?key=b9a8423ba82179e793ae4a5b1096f541"><i class="fab fa-youtube"></i></a>
-<a class="social-icon" href="https://perilastronaut.com/drgbi6nk3?key=b9a8423ba82179e793ae4a5b1096f541"><i class="fab fa-facebook"></i></a>
-<a class="social-icon" href="https://perilastronaut.com/drgbi6nk3?key=b9a8423ba82179e793ae4a5b1096f541"><i class="fab fa-twitter"></i></a>
-</div>
-</nav>
-<p class="copyright">Dashflix 2024. All rights reserved.</p>
-</footer>
 """
 
 def remove_block_from_html(directory, block):
