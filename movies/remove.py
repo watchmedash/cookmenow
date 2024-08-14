0b98bd2d8ae4da25c85de3b914e7adc9a6bc588e
watchmedash <mojazer2017@gmail.com>
2024-08-14 17:17:13 +0400

    up

--- movies/remove.py
+++ movies/remove.py
@@ -1,10 +1,10 @@
 import os
 
 # Directory containing your HTML files
-directory = r'C:\Users\HP\Documents\GitHub\cookmenow\movies'  # Replace with your actual directory path
+directory = r'C:\Users\mojaz\Documents\GitHub\cookmenow\movies'  # Replace with your actual directory path
 
 # Line to be removed
-line_to_remove = '<script async="" crossorigin="anonymous" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2412399424552673"></script>\n'
+line_to_remove = '<script src="https://alwingulla.com/88/tag.min.js" data-zone="88676" async data-cfasync="false"></script>\n'
 
 def remove_font_links(directory):
     for root, dirs, files in os.walk(directory):
--- movies/replacet.py
+++ movies/replacet.py
@@ -1,13 +1,26 @@
 import os
-import fileinput
 
 def replace_text_in_files(directory, old_text, new_text):
-    for filename in os.listdir(directory):
-        if filename.endswith(".html"):
-            filepath = os.path.join(directory, filename)
-            with fileinput.FileInput(filepath, inplace=True) as file:
-                for line in file:
-                    print(line.replace(old_text, new_text), end='')
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.html'):
+                filepath = os.path.join(root, file)
+                try:
+                    with open(filepath, 'r') as f:
+                        content = f.read()
 
-# Replace "Mercenary Enrollment" with "Overgeared" in all HTML files in the current directory
-replace_text_in_files('.', '</title><style><div class="content">', '</title><div class="content">')
+                    new_content = content.replace(old_text, new_text)
+
+                    with open(filepath, 'w') as f:
+                        f.write(new_content)
+                        print(f"Replaced text in {filepath}")
+
+                except Exception as e:
+                    print(f"Error processing {filepath}: {e}")
+
+# Call the function to replace text recursively
+replace_text_in_files(
+    '.',
+    '<script src="https://alwingulla.com/88/tag.min.js" data-zone="88676" async data-cfasync="false"></script>',
+    "<script type='text/javascript' src='//perilastronaut.com/27/b0/7b/27b07be5dbd507b6465940348cb90da5.js'></script>"
+)
