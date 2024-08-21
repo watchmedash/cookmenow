839ef8239084cb8cccb9b3046f6bf8d0ae51c0bc
watchmedash <mojazer2017@gmail.com>
2024-08-21 08:51:32 +0400

    up

--- movies/add.py
+++ movies/add.py
@@ -1,14 +1,14 @@
 import os
 
 # Define the script to be added
-script_code = '<meta content="origin" name="referrer"/>'
+script_code = '<div class="image-contained"><a href="https://anime-x.top"><img src="https://i.postimg.cc/wMqsKcWp/anime-ad.gif" alt="Watch Anime Online Free"></a></div>'
 
 # Get the current directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Loop through all files in the directory
 for filename in os.listdir(current_dir):
-    # Check if the file is an HTML file
+    # Process only HTML files
     if filename.endswith(".html"):
         filepath = os.path.join(current_dir, filename)
         try:
@@ -16,16 +16,16 @@ for filename in os.listdir(current_dir):
             with open(filepath, 'r', encoding='utf-8') as file:
                 content = file.read()
 
-            # Find the position of the </head> tag
-            position = content.rfind('</head>')
+            # Find the position of the <div class="content"> tag
+            position = content.rfind('<div class="content">')
 
             # If the tag is found, insert the script code before it
             if position != -1:
                 new_content = content[:position] + script_code + "\n" + content[position:]
-                # Write the new content back to the file
+                # Write the modified content back to the file
                 with open(filepath, 'w', encoding='utf-8') as file:
                     file.write(new_content)
         except Exception as e:
-            print(f"An error occurred while processing {filename}: {e}")
+            print(f"Error processing {filename}: {e}")
 
-print("Script code added to all HTML files.")
+print("Script code added to all applicable HTML files.")
