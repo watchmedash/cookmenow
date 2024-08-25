174270d255175f0a2514eb21c652157b1d5dbbea
watchmedash <mojazer2017@gmail.com>
2024-08-25 08:04:41 +0400

    up

--- movies/remove.py
+++ movies/remove.py
@@ -4,16 +4,16 @@ import os
 directory = r'C:\Users\HP\Documents\GitHub\cookmenow\movies'  # Replace with your actual directory path
 
 # Line to be removed
-line_to_remove = "<script type='text/javascript' src='//perilastronaut.com/c4/d5/02/c4d502002b9c4e29427b68a75921dd77.js'></script>\n"
+line_to_remove = '<script type="text/javascript" src="//perilastronaut.com/c4/d5/02/c4d502002b9c4e29427b68a75921dd77.js"></script>\n'
 
 def remove_font_links(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.html'):
                 filepath = os.path.join(root, file)
-                with open(filepath, 'r') as f:
+                with open(filepath, 'r', encoding='utf-8') as f:
                     lines = f.readlines()
-                with open(filepath, 'w') as f:
+                with open(filepath, 'w', encoding='utf-8') as f:
                     for line in lines:
                         if line != line_to_remove:
                             f.write(line)
