48c1f4156b8ebb0835e2318328c606602c7e6f55
watchmedash <mojazer2017@gmail.com>
2024-08-25 07:15:26 +0400

    up

--- movies/remove.py
+++ movies/remove.py
@@ -1,10 +1,10 @@
 import os
 
 # Directory containing your HTML files
-directory = r'C:\Users\mojaz\Documents\GitHub\cookmenow\movies'  # Replace with your actual directory path
+directory = r'C:\Users\HP\Documents\GitHub\cookmenow\movies'  # Replace with your actual directory path
 
 # Line to be removed
-line_to_remove = '<script src="https://alwingulla.com/88/tag.min.js" data-zone="88676" async data-cfasync="false"></script>\n'
+line_to_remove = "<script type='text/javascript' src='//perilastronaut.com/c4/d5/02/c4d502002b9c4e29427b68a75921dd77.js'></script>\n"
 
 def remove_font_links(directory):
     for root, dirs, files in os.walk(directory):
