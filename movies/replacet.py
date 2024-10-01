813de435a6686db3817ba640f19996061d2c184e
watchmedash <mojazer2017@gmail.com>
2024-10-01 17:24:32 +0400

    up

--- movies/replacet.py
+++ movies/replacet.py
@@ -1,74 +1,13 @@
 import os
-import re
+import fileinput
 
-def replace_two_divs_in_files(directory, old_div_pattern, new_div_content):
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.html'):
-                filepath = os.path.join(root, file)
-                try:
-                    with open(filepath, 'r', encoding='utf-8') as f:
-                        content = f.read()
+def replace_text_in_files(directory, old_text, new_text):
+    for filename in os.listdir(directory):
+        if filename.endswith(".html"):
+            filepath = os.path.join(directory, filename)
+            with fileinput.FileInput(filepath, inplace=True) as file:
+                for line in file:
+                    print(line.replace(old_text, new_text), end='')
 
-                    # Find and print any matches
-                    matches = old_div_pattern.findall(content)
-                    if matches:
-                        print(f"Found matches in {filepath}:")
-                        for match in matches:
-                            print(match)
-                    else:
-                        print(f"No matches found in {filepath}")
-
-                    # Replace the old div pattern with new content
-                    new_content = old_div_pattern.sub(new_div_content, content)
-
-                    # If changes were made, write the new content
-                    if new_content != content:
-                        with open(filepath, 'w', encoding='utf-8') as f:
-                            f.write(new_content)
-                        print(f"Replaced text in {filepath}")
-
-                except Exception as e:
-                    print(f"Error processing {filepath}: {e}")
-
-# Regular expression pattern to match exactly two <div class="image-contained"> blocks
-old_div_pattern = re.compile(r'''
-    <div\s+class="image-contained">\s*   # Match the first div
-    <a\s+href="https://go\.dashflix\.top/free50dollars">\s*
-    <img[^>]*?>\s*</a>\s*</div>\s*       # Match the img inside the a tag
-    \s*                                  # Allow optional whitespace or newlines between blocks
-    <div\s+class="image-contained">\s*   # Match the second div
-    <a\s+href="https://go\.dashflix\.top/free50dollars">\s*
-    <img[^>]*?>\s*</a>\s*</div>
-''', re.DOTALL | re.VERBOSE)
-
-# New content to replace the matched divs
-new_div_content = '''
-<div class="terra">
-    <script type="text/javascript">
-        atOptions = {
-            'key' : '2a96efd2197a238b07deb35984b19d75',
-            'format' : 'iframe',
-            'height' : 60,
-            'width' : 468,
-            'params' : {}
-        };
-    </script>
-    <script type="text/javascript" src="//perilastronaut.com/2a96efd2197a238b07deb35984b19d75/invoke.js"></script>
-</div>
-<div class="terra">
-    <script type="text/javascript">
-        atOptions = {
-            'key' : 'bf040c76d43b39046092658d84c88123',
-            'format' : 'iframe',
-            'height' : 50,
-            'width' : 320,
-            'params' : {}
-        };
-    </script>
-    <script type="text/javascript" src="//perilastronaut.com/bf040c76d43b39046092658d84c88123/invoke.js"></script>
-</div>
-'''
-
-# Call the function to replace the divs recursively in all files
-replace_two_divs_in_files('.', old_div_pattern, new_div_content)
+# Replace "Mercenary Enrollment" with "Overgeared" in all HTML files in the current directory
+replace_text_in_files('.', 'style.css', 'styles.css')
