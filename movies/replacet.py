2e4fa1b948a2d9606200df568cf352aeacefbd0a
watchmedash <mojazer2017@gmail.com>
2024-11-05 07:29:36 +0400

    up

--- movies/replacet.py
+++ movies/replacet.py
@@ -5,10 +5,10 @@ def replace_text_in_files(directory, old_text, new_text):
     for filename in os.listdir(directory):
         if filename.endswith(".html"):
             filepath = os.path.join(directory, filename)
-            # Using backup parameter to ensure safety during file replacement
-            with fileinput.FileInput(filepath, inplace=True, backup='.bak') as file:
+            # Specify utf-8 encoding to avoid encoding errors and avoid backup files
+            with fileinput.FileInput(filepath, inplace=True, encoding='utf-8') as file:
                 for line in file:
-                    # Replace the old_text with new_text and print each modified line
+                    # Replace old_text with new_text and print each modified line
                     print(line.replace(old_text, new_text), end='')
 
 # Replace "dashflix.xyz" with "dashflix.top" in all HTML files in the current directory
